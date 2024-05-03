60ce1924fca804d49449ed33d2b2557c906a0793
AbderrahimeEl <elmoutaouakil12345@gmail.com>
2024-05-03 20:05:49 +0100

    fix issue related to image displaying

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,14 +4,14 @@ import { contactDetails } from "../Details";
 function Contact() {
   const { email, phone } = contactDetails;
   return (
-    <main className="container mt-5 d-flex flex-column justify-content-center align-items-center">
-      <h1 className="display-3 mt-5 font-weight-bold text-center">
+    <main className="container mt-5 d-flex flex-column justify-content-center text-muted align-items-center">
+      <h3 className="display-3 mt-5 font-weight-bold text-center">
         You can contact me via:
-      </h1>
+      </h3>
       <h3 className="fs-3 mt-5 ">
         <a href={`mailto:${email}`}>{email}</a>
       </h3>
-      <span className="text-xl display-3 d-block my-3">or</span>
+      <span className="text-xl display-3 d-block my-3 text-muted">or</span>
       <h3 className="fs-3 mb-5">
         <a href={`tel:${phone}`}>{phone}</a>
       </h3>
